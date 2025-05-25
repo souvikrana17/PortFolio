13ba866f953ac46baa80f06cb6ed8ab9f9432bf6
Souvik Rana <110881476+Souvik-Rana@users.noreply.github.com>
2025-05-25 19:32:36 +0530

    Create StructuredData.tsx

---
+++ components/StructuredData.tsx
@@ -0,0 +1,23 @@
+const StructuredData = () => {
+    return (
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Souvik Rana",
+            "url": "https://souvik-rana.vercel.app",
+            "sameAs": [
+              "https://github.com/Souvik-Rana",
+              "https://www.linkedin.com/in/souvik-rana-sr17/",
+              "https://gravatar.com/souvikrana"
+            ]
+          }),
+        }}
+      />
+    );
+  };
+  
+  export default StructuredData;
+  
