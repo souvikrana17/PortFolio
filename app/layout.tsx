411c53378fa4c287b940f1ded78f89bded252c92
SOUVIK RANA <110881476+Souvik-Rana@users.noreply.github.com>
2025-06-10 10:14:48 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import StructuredData from "@/components/StructuredData"; // Import the componen
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Souvik Rana",
-  description: "Souvik Rana's Portfolio",
+  title: "SOUVIK RANA",
+  description: "SOUVIK RANA's Portfolio",
 };
 
 export default function RootLayout({
@@ -22,6 +22,21 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/logo.png" sizes="any" />
+        <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "SOUVIK RANA",
+  "url": "https://souvik-rana.vercel.app/",
+  "sameAs": [
+    "https://github.com/Souvik-Rana",
+    "https://www.linkedin.com/in/souvik-rana-sr17/",
+    "https://www.instagram.com/souvikrana17/",
+    "https://www.kaggle.com/souvikrana17"      
+  ]
+}
+</script>
+
       </head>
       <body className={inter.className}>
         <StructuredData /> {/* Include the structured data script */}
