fefecda856627dd75fea4002b17b6fd56c7d6407
SOUVIK RANA <rana.souvik17@gmail.com>
2025-06-16 12:40:25 +0530

    Update StructuredData.tsx

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -3,13 +3,13 @@ const StructuredData = () => {
     "@context": "https://schema.org",
     "@type": "Person",
     "name": "SOUVIK RANA",
-    "url": "https://souvik-rana.vercel.app",
+    "url": "https://souvikrana17.vercel.app",
     "sameAs": [
-      "https://github.com/Souvik-Rana",
-      "https://www.linkedin.com/in/souvik-rana-sr17/",
+      "https://github.com/souvikrana17",
+      "https://www.linkedin.com/in/souvikrana17/",
       "https://www.instagram.com/souvikrana17/",
       "https://www.kaggle.com/souvikrana17",
-      "https://gravatar.com/souvikrana"
+      "https://gravatar.com/souvikrana17"
     ]
   };
 
